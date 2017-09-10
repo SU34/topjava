026104488e0ed22cc896c3ab42a8169617a951f4
su-33 <jake33@mail.ru>
2017-09-10 13:04:36 +0700

    5 6 profile resolver

---
+++ src/test/java/ru/javawebinar/topjava/ActiveDbProfileResolver.java
@@ -0,0 +1,11 @@
+package ru.javawebinar.topjava;
+
+import org.springframework.test.context.ActiveProfilesResolver;
+
+//http://stackoverflow.com/questions/23871255/spring-profiles-simple-example-of-activeprofilesresolver
+public class ActiveDbProfileResolver implements ActiveProfilesResolver {
+
+    public String[] resolve(Class<?> aClass) {
+        return new String[]{Profiles.getActiveDbProfile()};
+    }
+}
